8f877d8bb02b098e48a412118c3013e32812a89b
刘诚勇 <liuchengyong@edaijia-inc.cn>
2018-05-17 17:34:33 +0800

    修改prebind
    Change-Id: I983227ad1a650453e1e90440167fa76f02d5fe2b

--- sendBook/gulp/dev.js
+++ sendBook/gulp/dev.js
@@ -15,7 +15,7 @@ var imagemin = require('gulp-imagemin');
 var config = require('../package.json');
 var rename = require("gulp-rename");
 
-var modules = ['index','login','prebind','address'];
+var modules = ['index','login','preBind','address'];
 
 var publicPath = '../public/',srcPath = '../src/';
 
