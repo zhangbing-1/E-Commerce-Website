8cf65eb5bf19a475e540d0fb7fc13805e6a96d7f
刘诚勇 <liucy@zongjie.com>
2018-06-23 15:44:25 +0800

    修改下载
    Change-Id: I4d35c0e97d413b0d62964b3bb9db13ffd28551af

--- appDownload/src/js/index.js
+++ appDownload/src/js/index.js
@@ -2,12 +2,12 @@ $(function() {
   var dom = $('#container'),androidPath = '';
   function bindEvent() {
     dom.on('click', '.btn', function(e) {
-      if(common.isAndroid && common.isWeixin){
+      if(!common.isPhone && common.isWeixin){
         dom.append(common.createShare());
-      }else if(common.isAndroid){
-        location.href = androidPath;
-      }else{
+      }else if(common.isPhone){
         location.href = 'https://itunes.apple.com/cn/app/%E7%88%B1%E6%80%BB%E7%BB%93/id1377039393?mt=8';
+      }else{
+        location.href = androidPath;
       }
     })
   }
