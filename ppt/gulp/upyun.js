6435012f99e66415f56c4971a70cade709ecc5c4
刘诚勇 <liucy@zongjie.com>
2018-11-05 17:33:02 +0800

    更新ppt更改
    Change-Id: Ib8b3f4329c5bc6c5f1a631b18fa6874e8280675c

---
+++ ppt/gulp/upyun.js
@@ -0,0 +1,21 @@
+/**
+ * npm run upyun pro //同步生产
+ * npm run upyun test //同步生产
+ *
+ */
+
+let upyun = require("upyun");
+let fs = require("fs")
+let path = require("path")
+
+const service = new upyun.Service('zongjie-web', 'liuchengyong', 'zongjie123');
+const client = new upyun.Client(service);
+
+let sourcePath = path.join(__dirname, '../src/');
+
+
+client.putFile('ppt', fs.createReadStream(sourcePath + fileName), {}).then(res => {
+  console.log(path + '===== success');
+}).catch(res => {
+  console.log(`${fileName} 上传失败`)
+})
