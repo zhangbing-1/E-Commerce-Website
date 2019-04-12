ee4fa236bb731f589f6a719f6247aca624d3983f
刘诚勇 <liucy@zongjie.com>
2019-04-12 16:47:29 +0800

    修改下载方式
    Change-Id: Id25d5ef40ec97e54a8d0d97cbb608b28cbece94b

--- appDownload/src/js/index.js
+++ appDownload/src/js/index.js
@@ -1,6 +1,17 @@
 $(function() {
   var dom = $('#container'),androidPath = '';
   function bindEvent() {
+
+    if(common.isPhone && common.isWeixin){
+      location.href = 'https://itunes.apple.com/cn/app/%E7%88%B1%E6%80%BB%E7%BB%93/id1377039393?mt=8';
+    }else if(common.isPhone){
+      location.href = 'aizongjie://100'
+    }else if(common.isWeixin){
+      common.createShare()
+    }else{
+      location.href = 'aizongjie://'
+    }
+
     dom.on('click', '.btn', function(e) {
       if(common.isPhone){
         location.href = 'https://itunes.apple.com/cn/app/%E7%88%B1%E6%80%BB%E7%BB%93/id1377039393?mt=8';
